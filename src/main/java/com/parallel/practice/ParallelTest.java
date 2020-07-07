3364dfeb361df29fdc9fc47af0e54ba5a13ed518
ramum <mrtchowdary@gmail.com>
2020-07-07 13:55:07 +0530

    Initial commit

---
+++ src/main/java/com/parallel/practice/ParallelTest.java
@@ -0,0 +1,46 @@
+package com.parallel.practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParallelTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeMethod
+	@Parameters("brow")
+	public void setUpMethod(String browser){
+		if(browser.equalsIgnoreCase("chrome")){
+			WebDriverManager.chromedriver().arch32().setup();
+			driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")){
+			WebDriverManager.firefoxdriver().arch32().setup();
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.seleniumeasy.com/test/");
+	}
+
+	@Test
+	public void testCase(){
+		System.out.println("Screen title is "+ driver.getTitle());
+	}
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+}
